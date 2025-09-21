3bf96b5ae8fbfb82d56860358d3cf20931b1c805
Zhou Wei <zhouwei@example.com>
2025-09-21 17:33:04 +0800

    fff

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+
+
+
+
+
+#[tokio::main]
+async fn main() {
+    let client = reqwest::Client::builder()
+        .proxy(reqwest::Proxy::http("http://45.139.132.19:58000").unwrap())
+        .build()
+        .unwrap();
+
+    let resp = client
+        .get("https://ifconfig.me")
+        .send()
+        .await
+        .unwrap();
+
+    let ip = resp.text().await.unwrap();
+    println!("Outgoing IP: {}", ip);
+}
